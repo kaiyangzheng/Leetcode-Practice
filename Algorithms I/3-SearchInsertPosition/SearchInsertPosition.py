ec9e2c8c3fb682b0e0bafd2fa426c74c9e671fa8
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2022-06-04 21:58:09 -0700

    SearchInsertPosition

---
+++ Algorithms I/3-SearchInsertPosition/SearchInsertPosition.py
@@ -0,0 +1,12 @@
+def searchInsert(nums: list[int], target: int) -> int:
+    left = 0
+    right = len(nums)-1
+    while (left <= right):
+        mid = left + (right-left)//2
+        if (nums[mid] == target):
+            return mid
+        if (nums[mid] > target):
+            right = mid-1
+        if (nums[mid] < target):
+            left = mid + 1
+    return left
