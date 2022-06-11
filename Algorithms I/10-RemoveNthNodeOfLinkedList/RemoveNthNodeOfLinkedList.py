3a8ea53a6dcc38267308031324addb9a2ea5b94e
Kaiyang Zheng <kaiyangzheng@Kaiyangs-MacBook-Pro.local>
2022-06-10 18:21:58 -0700

    day 5

---
+++ Algorithms I/10-RemoveNthNodeOfLinkedList/RemoveNthNodeOfLinkedList.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        current = head
+        length = 1
+        while(current.next != None):
+            current = current.next;
+            length += 1
+        if length == 1:
+            head = None;
+            return head;
+        if length-n-1 < 0:
+            head = head.next
+            return head
+        current = head
+        for i in range(length-n-1):
+            current = current.next
+        current.next = current.next.next
+        return head
